d7684350ca745e0452e82f63412aa29b25857f94
莫本姬 <2719543232@qq.com>
2022-06-26 19:16:48 +0000

    可视化混淆矩阵、分类报告、精确度

---
+++ test1.py
@@ -0,0 +1,277 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+
+np.random.seed(42)
+
+data_dir = './download'
+train_path = './download/Train'
+test_path = './download/Test'
+
+# images to 30x30x3
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
+
+# Train文件夹下的文件数量
+NUM_CATEGORIES = len(os.listdir(train_path))
+NUM_CATEGORIES
+print(NUM_CATEGORIES)
+NUM_CATEGORIES = NUM_CATEGORIES - 1
+print(NUM_CATEGORIES)
+
+# Label 重写
+classes = {0: 'Speed limit (20km/h)',
+           1: 'Speed limit (30km/h)',
+           2: 'Speed limit (50km/h)',
+           3: 'Speed limit (60km/h)',
+           4: 'Speed limit (70km/h)',
+           5: 'Speed limit (80km/h)',
+           6: 'End of speed limit (80km/h)',
+           7: 'Speed limit (100km/h)',
+           8: 'Speed limit (120km/h)',
+           9: 'No passing',
+           10: 'No passing veh over 3.5 tons',
+           11: 'Right-of-way at intersection',
+           12: 'Priority road',
+           13: 'Yield',
+           14: 'Stop',
+           15: 'No vehicles',
+           16: 'Veh > 3.5 tons prohibited',
+           17: 'No entry',
+           18: 'General caution',
+           19: 'Dangerous curve left',
+           20: 'Dangerous curve right',
+           21: 'Double curve',
+           22: 'Bumpy road',
+           23: 'Slippery road',
+           24: 'Road narrows on the right',
+           25: 'Road work',
+           26: 'Traffic signals',
+           27: 'Pedestrians',
+           28: 'Children crossing',
+           29: 'Bicycles crossing',
+           30: 'Beware of ice/snow',
+           31: 'Wild animals crossing',
+           32: 'End speed + passing limits',
+           33: 'Turn right ahead',
+           34: 'Turn left ahead',
+           35: 'Ahead only',
+           36: 'Go straight or right',
+           37: 'Go straight or left',
+           38: 'Keep right',
+           39: 'Keep left',
+           40: 'Roundabout mandatory',
+           41: 'End of no passing',
+           42: 'End no passing veh > 3.5 tons'}
+
+folders = os.listdir(train_path)
+
+train_number = []
+class_num = []
+
+for folder in folders:
+    if folder == '.ipynb_checkpoints':
+        continue
+    print(folder)
+    print(classes[int(folder)])
+    train_files = os.listdir(train_path + '/' + folder)
+    train_number.append(len(train_files))
+    class_num.append(classes[int(folder)])
+
+# 根据每个类中的图像数量对数据集进行排序
+zipped_lists = zip(train_number, class_num)
+sorted_pairs = sorted(zipped_lists)
+tuples = zip(*sorted_pairs)
+train_number, class_num = [list(tuple) for tuple in tuples]
+
+# 绘制每个类中的图像数量
+plt.figure(figsize=(21, 10))
+plt.bar(class_num, train_number)
+plt.xticks(class_num, rotation='vertical')
+plt.show()
+
+# 测试数据的 25 张随机图像
+import random
+from matplotlib.image import imread
+
+test = pd.read_csv(data_dir + '/Test.csv')
+imgs = test["Path"].values
+
+plt.figure(figsize=(25, 25))
+
+for i in range(1, 26):
+    plt.subplot(5, 5, i)
+    random_img_path = data_dir + '/' + random.choice(imgs)
+    rand_img = imread(random_img_path)
+    plt.imshow(rand_img)
+    plt.grid()
+    plt.xlabel(rand_img.shape[1], fontsize=20)  # 图片宽度
+    plt.ylabel(rand_img.shape[0], fontsize=20)  # 图片高度
+
+# 训练数据：
+image_data = []
+image_labels = []
+
+for i in range(NUM_CATEGORIES):
+    path = data_dir + '/Train/' + str(i)
+    if i == 43:
+        continue
+    images = os.listdir(path)
+
+    for img in images:
+        try:
+            image = cv2.imread(path + '/' + img)
+            image_fromarray = Image.fromarray(image, 'RGB')
+            resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+            image_data.append(np.array(resize_image))
+            image_labels.append(i)
+        except:
+            print("Error in " + img)
+
+# 将 list 更改为 numpy 数组
+image_data = np.array(image_data)
+image_labels = np.array(image_labels)
+
+print(image_data.shape, image_labels.shape)
+
+# 打乱数据集
+
+shuffle_indexes = np.arange(image_data.shape[0])
+np.random.shuffle(shuffle_indexes)
+image_data = image_data[shuffle_indexes]
+image_labels = image_labels[shuffle_indexes]
+
+# 将数据拆分为训练集和验证集
+X_train, X_val, y_train, y_val = train_test_split(image_data, image_labels, test_size=0.3, random_state=42,
+                                                  shuffle=True)
+# 图形归一化
+X_train = X_train / 255
+X_val = X_val / 255
+
+print("X_train.shape", X_train.shape)
+print("X_valid.shape", X_val.shape)
+print("y_train.shape", y_train.shape)
+print("y_valid.shape", y_val.shape)
+
+# 独热编码：
+y_train = keras.utils.to_categorical(y_train, NUM_CATEGORIES)
+y_val = keras.utils.to_categorical(y_val, NUM_CATEGORIES)
+# 分为了43个独热编码：
+print(y_train.shape)
+print(y_val.shape)
+
+# 模型的训练：
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
+                        input_shape=(IMG_HEIGHT, IMG_WIDTH, channels)),
+    keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(rate=0.5),
+
+    keras.layers.Dense(42, activation='softmax')
+])
+
+lr = 0.001
+epochs = 30
+
+opt = Adam(lr=lr, decay=lr / (epochs * 0.5))
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+# 扩充数据并训练模型
+aug = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode="nearest")
+
+history = model.fit(aug.flow(X_train, y_train, batch_size=32), epochs=epochs, validation_data=(X_val, y_val))
+
+model.save('traffic_classifier.h5')
+
+# 评估模型：
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+# 加载测试数据并运行预测
+test = pd.read_csv(data_dir + '/Test.csv')
+
+labels = test["ClassId"].values
+imgs = test["Path"].values
+
+data = []
+
+for img in imgs:
+    try:
+        image = cv2.imread(data_dir + '/' + img)
+        image_fromarray = Image.fromarray(image, 'RGB')
+        resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+        data.append(np.array(resize_image))
+    except:
+        print("Error in " + img)
+X_test = np.array(data)
+X_test = X_test / 255
+
+pred = model.predict(X_test)
+
+# 测试集的精确度：
+print('Test Data accuracy: ', accuracy_score(labels, pred) * 100)
+
+# 可视化混淆矩阵：
+from sklearn.metrics import confusion_matrix
+
+cf = confusion_matrix(labels, pred)
+
+import seaborn as sns
+
+df_cm = pd.DataFrame(cf, index=classes, columns=classes)
+plt.figure(figsize=(20, 20))
+sns.heatmap(df_cm, annot=True)
+
+# 分类报告：
+from sklearn.metrics import classification_report
+
+print(classification_report(labels, pred))
+
+# 测试数据的预测： 获取预测的精确度
+plt.figure(figsize=(25, 25))
+
+start_index = 0
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    prediction = pred[start_index + i]
+    actual = labels[start_index + i]
+    col = 'g'
+    if prediction != actual:
+        col = 'r'
+    plt.xlabel('Actual={} || Pred={}'.format(actual, prediction), color=col)
+    plt.imshow(X_test[start_index + i])
+plt.show()
